b5ab85ca72377148a7c97eb764d144f2011a808d
Chris Brown <chris@2pha.com>
2022-12-04 18:12:40 +1000

    Added some more postcss stuff.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 let plugins = [
   require('postcss-import'),
   require('postcss-nested'),
+  require('postcss-custom-media'),
+  require("postcss-each"),
+  require('postcss-extend'), // be careful, extend does not support nesting.
   require('postcss-preset-env')({ browsers: ['last 2 versions and not dead and > .5%'] }), // check at: https://browserslist.dev/
   require('postcss-focus'),
   require('postcss-mixins'),
